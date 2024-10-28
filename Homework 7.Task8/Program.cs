c6e1b1333e1be3e5ec934c5b627779019b23728c
Tteean <nailato@code.edu.az>
2024-10-28 13:51:29 +0400

    new task added

--- Homework 7.Task8/Program.cs
+++ Homework 7.Task8/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(EmailAdressDomain("test@code.edu.az"));
         }
 
         /*8.Verilmiş email dəyərinin domain hissəni qaytaran metod 
          * (test@code.edu.az email-i daxil edilsə code.edu.az hiss'sini qaytarmalıdır)*/
-
+        public static string EmailAdressDomain(string email)
+        {
+            var domain  = email.Substring(email.IndexOf("@") + 1);
+            return domain;
+        }
     }
 }
